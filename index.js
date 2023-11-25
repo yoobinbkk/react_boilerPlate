e6e1ed2e51f5b988b68b4af3534decf0209a5aff
Yim Yoobin <yoobinbkk@naver.com>
2023-11-25 11:20:16 +0900

    mongodb uri setting

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const {User} = require('./models/User')
+
+const config = require('./config/key')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+// application/json
+app.use(bodyParser.json())
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yoobinbkk:ajaj1234@boilerplate.pcj4kqf.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI)
+.then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/', (req, res) => res.send('Hello World! ㅋㅋㅋㅋㅋㅋ'))
+app.post('/register', (req, res) => {
+    // 회원 가입할 때 필요한 정보들을 client 에서 가져오면 그것들을 데이터베이스에 넣어준다
+    try {
+        const user = new User(req.body)
+        user.save().then(() => {
+            return res.status(200).json({success: true})
+        })
+    } catch(err) {
+        return res.json({success: false, err})
+    }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
